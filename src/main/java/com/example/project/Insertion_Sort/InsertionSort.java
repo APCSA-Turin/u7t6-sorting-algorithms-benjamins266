fa53b9d693e05e6fc405888621b0b5e7d6b23aa4
benjamins266 <benjamins266@nycstudents.net>
2025-02-26 16:12:59 +0000

    InsertionSort

--- src/main/java/com/example/project/Insertion_Sort/InsertionSort.java
+++ src/main/java/com/example/project/Insertion_Sort/InsertionSort.java
@@ -8,22 +8,61 @@ public class InsertionSort {
 
     // PART A. implementing insertion sort
     public static int[] insertionSort(int[] elements) {
-       
+       for(int i = 1; i<elements.length; i++){
+        int value = elements[i];
+        int j = i;
+        while(j != 0 && value<elements[j-1]){
+            int temp = elements[j-1]; 
+            elements[j] = temp;
+            elements[j-1] = value;
+            j--;
+        }
+       }
         return elements;
     }
 
    
     public static void selectionSort(int[] elements) {
-    
+        for(int i = 0; i<elements.length; i++){
+            int lowestValue = i;
+            for(int j = i; j<elements.length; j++){
+                if(elements[j]<elements[lowestValue]){
+                    lowestValue = j;
+                }
+            }
+            int temp = elements[i];
+            elements[i] = elements[lowestValue];
+            elements[lowestValue] = temp;
+        }
     }
 
     // PART B. sorting a 1000-word list
     public static ArrayList<String> insertionSortWordList(ArrayList<String> words) {
-    
+        for(int i = 1; i<words.size(); i++){
+            String value = words.get(i);
+            int j = i;
+            while(j != 0 && value.compareTo(words.get(j-1))<0){
+                String temp = words.get(j-1); 
+                words.set(j,temp);
+                words.set(j-1,value);
+                j--;
+            }
+           }
         return words;
     }
 
     public static void selectionSortWordList(ArrayList<String> words) {
+        for(int i = 0; i<words.size(); i++){
+            int lowestValue = i;
+            for(int j = i; j<words.size(); j++){
+                if(words.get(j).compareTo(words.get(lowestValue))<0){
+                    lowestValue = j;
+                }
+            }
+            String temp = words.get(i);
+            words.set(i, words.get(lowestValue));
+            words.set(lowestValue,temp);
+        }    
     
     }
 
